939a08fa354a1875927c82d1a8c35d12e4e85651
InnaOstriakIO <innaostriak@gmail.com>
2022-06-20 02:18:15 -0700

    what to do?

---
+++ Hello World.py
@@ -0,0 +1,20 @@
+a1 = 'I_'
+a2 = 'can_'
+a3 = 'do_'
+a4 = 'it'
+print(a1+a2+a3+a4)
+
+r4 = "Classroom story"
+print(r4)
+
+y1 = "The students in the class"
+y2 = "It seems nobody in the class"
+y3 = "We can finish earlier today!"
+
+if 8 >= 20:
+    print("if we have 20 students,The teacher says:", y1)
+elif 8 == 8:
+    print( "if we have 8 students,The teacher says:", y3)
+else:
+    print("if we have 1 student,The teacher says:", y2)
+
